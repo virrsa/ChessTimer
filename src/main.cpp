8d4dd69cb87ded9c4424160d952be75d2cc3fc3c
Purple-GH <apurplebanana1@gmail.com>
2023-03-26 10:42:07 -0400

    INT0 changes LED based on player turn

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,8 @@ char text[MAX_TEXT]; // for USART input
 // Output values to display numbers on 7-seg
 byte digits[ARRAY_SIZE_DECIMAL] = {0xFC, 0x60, 0xDA, 0xF2, 0x66, 0xB6, 0xBE, 0xE0, 0xFE, 0xF6, // 0 - 9
                                    0xEE, 0x3E, 0x9C, 0x7A, 0x9E, 0x8E}; // A - F
+volatile bool change = false; // Keep track of whether there was a change, to perform actions in the while loop as needed
+volatile bool currPlayer = false; // True if P1's turn, false if P2's - Initial button press flips from P2 to start with P1
 
 int main() {
 
@@ -35,7 +37,7 @@ int main() {
 
   while(1) {
     /* 
-    //For testing the different components
+    //For testing the different components without interrupts
 
     change_led(true);
     _delay_ms(500);
@@ -49,10 +51,21 @@ int main() {
     _delay_ms(100);
     toggle_buzzer(false);
     */
+
+    if(change) { // If something is different for this loop, perform actions as needed
+      change = false; // Reset the flag because the change is being addresses
+      change_led(currPlayer); // Make sure the LED is lit up for the correct player
+    }
   }
 }
 
 // Button input interrupt to swap player turns and timer countdowns
 ISR(INT0_vect) {
-
+  change = true; // Something changed, so let the main game loop know
+  if(currPlayer) { // If player 1 ended their turn, it's now player 2's turn
+    currPlayer = false;
+  }
+  else { // If player 2 ended their turn, it's now player 1's turn
+    currPlayer = true;
+  }
 }
